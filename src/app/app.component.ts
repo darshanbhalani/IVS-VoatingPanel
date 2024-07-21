e6a213cae6e007aa3ac4fea7f57ed86d9c0f68f9
DARSHAN BHALANI <darsganbhalanipatel@gmail.com>
2024-07-21 23:53:17 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,11 @@ import { LayoutComponent } from './components/layout/layout.component';
 import { LayoutService } from './services/layout.service';
 import { CommonModule } from '@angular/common';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-
+import { GridModule } from '@progress/kendo-angular-grid';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [LayoutComponent,CommonModule,PageNotFoundComponent],
+  imports: [LayoutComponent,CommonModule,PageNotFoundComponent,GridModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
